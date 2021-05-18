bf397df25744beb5f8bc41a73bdb3212d2952ba4
RobertoZeppilli <robzep171@gmail.com>
2021-05-18 14:41:55 +0200

    Vue Slider

---
+++ js/script.js
@@ -0,0 +1,35 @@
+let app = new Vue(
+    {
+        el: "#root",
+        data: {
+            images: [
+                "img/photo1.jpg",
+                "img/photo2.jpg",
+                "img/photo3.jpg",
+                "img/photo4.jpg",
+                "img/photo5.jpg"
+            ],
+            imageIndex: 0,
+            iconIndex: 0
+        },
+        methods: {
+            prevImage: function () {
+                this.imageIndex--;
+
+                if (this.imageIndex == -1) {
+                    this.imageIndex = this.images.length - 1;
+                }
+            },
+            nextImage: function () {
+                this.imageIndex++;
+
+                if (this.imageIndex == this.images.length) {
+                    this.imageIndex = 0;
+                }
+            },
+            circleClick: function () {
+                
+            }
+        }
+    }
+);
